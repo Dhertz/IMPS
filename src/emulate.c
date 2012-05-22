65c7b1f8ec434fc059cfdf1dc07d065934242eee
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-05-22 16:40:22 +0100

    Work on state data structure - memory done?

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct state {
+	short *mem;
+	short pc;
+	int reg[32];
+};
+
+struct state init(struct state st) {
+	st.mem = calloc(4096, sizeof(short));
+
+	if (st.mem == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	return st;
+}
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+	struct state st;
+	st = init(st);
+	
+	printf("%i\n", st.mem[0]);
+
+	free(st.mem);
+ 	return EXIT_SUCCESS;
 }
