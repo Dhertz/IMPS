f63eb0ef9bc1e14f7617694b20dc3fadcdcd7ec7
Owen Davies <tod11@doc.ic.ac.uk>
2012-05-31 16:04:44 +0100

    Added iterator start, end and insert elem

--- src/symtable.c
+++ src/symtable.c
@@ -13,6 +13,8 @@ typedef struct table_node {
 	node_t *next;
 } node_t;
 
+typedef node_t iterator;
+
 void *allocElem(void) {
 	node_t *new = malloc(sizeof(node_t));
 	if (elem == NULL) {
@@ -26,11 +28,30 @@ void freeElem(node_t *elem) {
 	free(elem);
 }
 
-void init(struct table *l) {
-	l->head = allocElem();
-	l->foot = allocElem();
-	l->head->prev = NULL;
-	l->foot->next = NULL;
-	l->head->next = l->foot;
-	l->foot->prev = l->head;
+void init(struct table *t) {
+	t->head = allocElem();
+	t->foot = allocElem();
+	t->head->prev = NULL;
+	t->foot->next = NULL;
+	t->head->next = t->foot;
+	t->foot->prev = t->head;
+}
+
+iterator start(struct table *t) {
+	return t->head->next;
+}
+
+iterator end(struct table *t) {
+	return t->foot;
+}
+
+void insert(struct table *t, iterator i, string key, int value) {
+	node_t new = allocElem();
+	new->key = key;
+	new->value = value;
+	
+	new->prev = iter->prev;
+	new->next = iter;
+	iter->prev->next = new;
+	iter->prev = new;
 }
