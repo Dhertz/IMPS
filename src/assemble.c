5746933a011faea0016038f9c4c18b8384f16c44
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-06-05 16:30:56 +0100

    Written writeInstruction method, see main for usage.

--- src/assemble.c
+++ src/assemble.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <assert.h>
 #include "symtable.h"
 
 char *getLabel(char *str) {
@@ -18,52 +20,53 @@ void addMnemonics(table *t) {
 	}
 }
 
+void writeInstruction(uint32_t opCode, uint32_t *data, FILE *fp) {
+	assert (0 <= opCode && opCode <= 18);
+	uint32_t inst = opCode << 26;
+
+	if (opCode == 15 || opCode == 17) {
+		/* J-type instructions */
+		uint32_t addr = data[0];
+		inst |= addr;
+	} else if (opCode == 1 || opCode == 3 || opCode == 5 || opCode == 16 || opCode == 18) {
+		/* R-type instructions */
+		uint32_t r1 = data[0];
+		uint32_t r2 = data[1];
+		uint32_t r3 = data[2];
+		inst |= (r1 << 21);
+		inst |= (r2 << 16);
+		inst |= (r3 << 11);
+	} else if (opCode != 0) {
+		/* I-type instructions */
+		uint32_t r1 = data[0];
+		uint32_t r2 = data[1];
+		uint32_t val = data[2];
+		inst |= (r1 << 21);
+		inst |= (r2 << 16);
+		inst |= val;
+	}
+
+	fwrite(&inst, 4, 1, fp);
+}
+
 int main(int argc, char **argv) {
-	FILE *in;
-	long size;
-	char *buffer;
-	
-	if ((in = fopen(argv[1], "rb")) == NULL) {
+	FILE *out;
+
+	if ((out = fopen(argv[1], "wb")) == NULL) {
 		perror("fopen");
 		exit(EXIT_FAILURE);
 	}
-	
-	fseek(in, 0, SEEK_END);
-	size = ftell(in);
-	fseek(in, 0, SEEK_SET);
-	
-	buffer = malloc(size * 4);
-	fread(buffer, 4, size, in);
-	
-	table symbols;
-	init(&symbols);
-	
-	const char *delim = "\n";
-	char *token = strtok(buffer, delim);
-	int offset = 0;
-	
-	while (token != NULL) {
-		if (strchr(token, ':') != '\0') {
-			insertFront(&symbols, getLabel(token), offset);
-		}
-		/* 0x00D = Carriage return - might need more coverage here */ 
-		if (token[0] != 0x00D) {
-			offset += 4;
-		}
-		token = strtok(NULL, delim);
-	}
-	
-	addMnemonics(&symbols);
-	
-	for(iterator i = start(&symbols); i != end(&symbols); i = next(i)) {
-		printf("%s -> %i\n", getKey(i), get(&symbols, getKey(i)));
-	}
 
-	freeTable(&symbols);
-	
-	fclose(in);
-	free(buffer);
+	/* Test input of beq $5 $8 24 */
+	uint32_t opCode = 9;
+	uint32_t *data = malloc(1 * sizeof(int));
+	data[0] = 5;
+	data[1] = 8;
+	data[2] = 24;
 
- 	return EXIT_SUCCESS;
-	
+	writeInstruction(opCode, data, out);
+
+	free(data);
+	fclose(out);
+	return EXIT_SUCCESS;	
 }
