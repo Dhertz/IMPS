c04af193a3b1bb82357252e665ff1c2e91880a61
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-06-16 17:13:41 +0100

    PPM working.

--- src/ppm.s
+++ src/ppm.s
@@ -33,18 +33,33 @@ maxValue:    lw $1 $0 two
              lw $1 $0 space
              out $1           - Output "255 " to stdout
 
-
+             addi $1 $0 255   - $1 = 255
              addi $2 $0 0     - $2 = 0
-             addi $3 $0 2048  - $3 = 2048
-twoPixels:   addi $1 $0 255
+             addi $3 $0 32    - $3 = 32
+
+twoRows:     addi $4 $0 0     - $4 = 0
+whiteBlack:  out $1
+             out $1
+             out $1           - Output white pixel
+             out $0
+             out $0
+             out $0           - Output black pixel
+             addi $4 $4 1     - $4++
+             beq $4 $3 2      - Skip if row finished,
+             jmp whiteBlack   -   otherwise loop
+
+             addi $4 $0 0     - $4 = 0
+blackWhite:  out $0
+             out $0
+             out $0           - Output black pixel
              out $1
              out $1
-             out $1
-             addi $1 $0 0
-             out $1
-             out $1
-             out $1
-             addi $2 $2 1
-             beq $2 $3 2
-             jmp twoPixels
+             out $1           - Output white pixel
+             addi $4 $4 1     - $4++
+             beq $4 $3 2      - Skip if row finished,
+             jmp blackWhite   -   otherwise loop
+
+             addi $2 $2 1     - $2++
+             beq $2 $3 2      - Skip if image finished,
+             jmp twoRows      -   otherwise loop
              halt
