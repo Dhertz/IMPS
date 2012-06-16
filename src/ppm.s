e39e9adbe3fba374d087af23b8d4cae7f72fcdf4
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-06-16 15:25:17 +0100

    Added maximum colour value to ppm.

--- src/ppm.s
+++ src/ppm.s
@@ -1,9 +1,9 @@
 jmp magicNumber
 
-p:     .fill 80 - ASCII for 'P'
-six:   .fill 54 - ASCII for '6'
 space: .fill 32 - ASCII for ' '
 four:  .fill 52 - ASCII for '4'
+six:   .fill 54 - ASCII for '6'
+p:     .fill 80 - ASCII for 'P'
 
 magicNumber: lw $1 $0 p
              out $1
@@ -11,7 +11,6 @@ magicNumber: lw $1 $0 p
              out $1
              lw $1 $0 space
              out $1           - Output "P6 " to stdout
-
 
              addi $3 $3 2     - $3 = 2
 widthHeight: lw $1 $0 six
@@ -23,4 +22,9 @@ widthHeight: lw $1 $0 six
              addi $2 $2 1     - $2++
              beq $2 $3 2      - Skip if outputted twice,
              jmp widthHeight  -   otherwise loop
+
+maxValue:    addi $1 $0 255
+             out $1           - Output 255 to stdout
+             lw $1 $0 space
+             out $1           - Output " " to stdout
              halt
