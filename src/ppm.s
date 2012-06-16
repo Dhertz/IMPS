603b22828f06562b49e8cb5b321e0c7378c39514
Charlie Hothersall-Thomas <cjh111@doc.ic.ac.uk>
2012-06-16 15:44:53 +0100

    ppm should now be working (I'm happy with the output file) but isn't working in image viewers - must have misinterpreted syntax from spec.

--- src/ppm.s
+++ src/ppm.s
@@ -12,7 +12,7 @@ magicNumber: lw $1 $0 p
              lw $1 $0 space
              out $1           - Output "P6 " to stdout
 
-             addi $3 $3 2     - $3 = 2
+             addi $3 $0 2     - $3 = 2
 widthHeight: lw $1 $0 six
              out $1
              lw $1 $0 four
@@ -27,4 +27,19 @@ maxValue:    addi $1 $0 255
              out $1           - Output 255 to stdout
              lw $1 $0 space
              out $1           - Output " " to stdout
+
+
+             addi $2 $0 0     - $2 = 0
+             addi $3 $0 2048  - $3 = 2048
+twoPixels:   addi $1 $0 255
+             out $1
+             out $1
+             out $1
+             addi $1 $0 0
+             out $1
+             out $1
+             out $1
+             addi $2 $2 1
+             beq $2 $3 2
+             jmp twoPixels
              halt
